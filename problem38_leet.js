50cc9409e5545a118ba54675312542b78ab48165
Anil Dabas <adabas@cheetahnetworks.com>
2024-09-01 18:09:14 -0400

    more leet code problems

---
+++ problem38_leet.js
@@ -0,0 +1,54 @@
+/*
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+*/
+
+var isPalindrome = function(s) {
+    let start = 0;
+    let end = s.length - 1;
+ 
+    let format = /[!@#$%^&*()_+\-=\[\]{};`':"\\|,.<>\/?]+/;
+ 
+    while (end >= start) {
+     if (format.test(s[start]) || s[start] === ' ') {
+         start++;
+         continue;
+     } 
+     if (format.test(s[end]) || s[end] === ' ') {
+         end--;
+         continue;
+     }
+     
+     if (s[start].toLowerCase() !== s[end].toLowerCase()) {
+         return false;
+     }
+ 
+     start++;
+     end--;
+    }
+ 
+    return true;
+ };
---
+++ problem39_leet.js
@@ -0,0 +1,66 @@
+/*
+    Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+*/
+
+var reverseWords = function(s) {
+    let word = '';
+    let result = '';
+    let j = s.length - 1;
+    let start = -1;
+
+    while (j >= 0) {
+        if (s[j] != ' ') {
+            if (start === -1) {
+                start = j;                
+            } 
+
+            word = s[j] + word;
+        } else {
+            if (start != -1) {
+                if (result === '') {
+                    result = result + word;
+                } else {
+                    result = result + ' ' + word;
+                }                
+                word = '';
+            }
+            start = -1;
+        }
+
+        j--;
+    }
+
+    if (word) {
+        if (result === '') {
+            result = result + word;
+        } else {
+                result = result + ' ' + word;
+        }  
+    }
+    
+    return result;
+};
